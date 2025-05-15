57af56f32ad434bb39b1d4b80ed750bd37523a9a
danielbelle <henrique.danielb@gmail.com>
2025-05-14 21:23:47 -0300

    0.1.11 - 11 config

--- resources/views/components/form.blade.php
+++ resources/views/components/form.blade.php
@@ -35,7 +35,7 @@
 <form method="POST" autocomplete="off" action="{{ route('process.input') }}">
   @csrf
 
-  <div class="lg:w-80">
+  <div class="md:w-2/3 mx-auto">
     <div class="flex flex-wrap">
 
       <div class="p-2 w-1/3">
